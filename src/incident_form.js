0c1c0b0a9251b829877905366a3e607bebd85bb5
Stefan Hall <stefanhall@SHAL003975.attlocal.net>
2023-09-23 23:21:20 -0400

    Added pagination to incident list

--- src/incident_form.js
+++ src/incident_form.js
@@ -132,6 +132,234 @@ function IncidentForm() {
     }
   };
 
+  const IncidentTitle = () => {
+    return (
+      <div className="mb-4">
+      <label
+        htmlFor="title"
+        className="block text-gray-600 font-bold mb-2"
+      >
+        Title:
+      </label>
+      <input
+        type="text"
+        id="title"
+        value={title}
+        onChange={handleTitleChange}
+        className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        required
+      />
+    </div>
+    )
+  };
+  const IncidentDescription = () => {
+    return (
+      <div className="mb-4">
+      <label
+        htmlFor="description"
+        className="block text-gray-600 font-bold mb-2"
+      >
+        Description:
+      </label>
+      <textarea
+        id="description"
+        value={description}
+        onChange={handleDescriptionChange}
+        className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        required
+      />
+    </div>
+    )
+  };
+  const OpsGeniePriority = () => {
+    return (
+      <div className="mb-4">
+      <label
+        htmlFor="priority"
+        className="block text-gray-600 font-bold mb-2"
+      >
+        Priority:
+      </label>
+      <select
+        id="priority"
+        value={priority}
+        onChange={handlePriorityChange}
+        className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        required
+      >
+        <option value="" disabled>
+          Select priority
+        </option>
+        <option value="P1">P1</option>
+        <option value="P2">P2</option>
+        <option value="P3">P3</option>
+        <option value="P4">P4</option>
+        <option value="P5">P5</option>
+      </select>
+    </div>
+    )
+  }
+  const IncidentImpact = () => {
+    return (
+      <div className="mb-4">
+      <label
+        htmlFor="impact"
+        className="block text-gray-600 font-bold mb-2"
+      >
+        Impact:
+      </label>
+      <select
+        id="impact"
+        value={impact}
+        onChange={handleImpactChange}
+        className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        required
+      >
+        <option value="" disabled>
+          Select impact
+        </option>
+        <option value="maintenance">Maintenance</option>
+        <option value="minor">Minor</option>
+        <option value="major">Major</option>
+        <option value="critical">Critical</option>
+      </select>
+    </div>
+    )
+  };
+
+  const AffectedComponents = () => {
+    return (
+      <div className="mb-4">
+      <label
+        htmlFor="componentIds"
+        className="block text-gray-600 font-bold mb-2"
+      >
+        Select Components (Ctrl+Click for multiple):
+      </label>
+      <select
+        id="componentIds"
+        multiple
+        value={componentIds}
+        onChange={(e) =>
+          setComponentIds(
+            [...e.target.selectedOptions].map((option) => option.value)
+          )
+        }
+        className="w-full h-32 px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        required
+      >
+        {components.map((component) => (
+          <option key={component.id} value={component.id}>
+            {component.name}
+          </option>
+        ))}
+      </select>
+    </div>
+    )
+  };
+  const SendNotifications = () => {
+    return (
+      <div className="mb-4">
+      <label
+        htmlFor="notifications"
+        className="block text-gray-600 font-bold mb-2"
+      >
+        Send Notifications?
+      </label>
+      <input
+        type="checkbox"
+        id="notifications"
+        checked={notifications}
+        onChange={() => setNotifications(!notifications)}
+        className="mr-2 leading-tight"
+      />
+      <span className="text-gray-700">Yes</span>
+    </div>
+    )
+  };
+  const OutageType = () => {
+    return (
+      <div className="mb-4">
+      <label
+        htmlFor="outageType"
+        className="block text-gray-600 font-bold mb-2"
+      >
+        Outage Type:
+      </label>
+      <select
+        id="outageType"
+        value={outageType}
+        onChange={handleOutageTypeChange}
+        className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        required
+      >
+        <option value="" disabled>
+          Select outage type
+        </option>
+        <option value="operational">Operational</option>
+        <option value="under_maintenance">Under Maintenance</option>
+        <option value="degraded_performance">Degraded Performance</option>
+        <option value="partial_outage">Partial Outage</option>
+        <option value="major_outage">Major Outage</option>
+      </select>
+    </div>
+    )
+  };
+  const Status = () => {
+    return (
+      <div className="mb-4">
+      <label
+        htmlFor="status"
+        className="block text-gray-600 font-bold mb-2"
+      >
+        Status:
+      </label>
+      <select
+        id="status"
+        value={status}
+        onChange={handleStatusChange}
+        className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        required
+      >
+        <option value="" disabled>
+          Select status
+        </option>
+        <option value="investigating">Investigating</option>
+        <option value="identified">Identified</option>
+        <option value="monitoring">Monitoring</option>
+        <option value="resolved">Resolved</option>
+      </select>
+    </div>
+    )
+  };
+
+  const IncidentCreationSubmitButton = () => {
+    return (
+      <button
+      type="submit"
+      className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none hover:bg-blue-700"
+    >
+      Create Incident
+    </button>
+    )
+  };
+
+  const CreateIncidentForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+      <IncidentTitle />
+      <IncidentDescription />
+      <OpsGeniePriority />
+      <IncidentImpact />
+      <AffectedComponents />
+      <SendNotifications />
+      <OutageType />
+      <Status />
+      <IncidentCreationSubmitButton />
+    </form>
+    )
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -158,182 +386,7 @@ function IncidentForm() {
         <h2 className="text-2xl font-semibold text-gray-800 mb-4">
           Create Incident
         </h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="title"
-              className="block text-gray-600 font-bold mb-2"
-            >
-              Title:
-            </label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={handleTitleChange}
-              className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="description"
-              className="block text-gray-600 font-bold mb-2"
-            >
-              Description:
-            </label>
-            <textarea
-              id="description"
-              value={description}
-              onChange={handleDescriptionChange}
-              className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
-
-          <div className="mb-4">
-            <label
-              htmlFor="priority"
-              className="block text-gray-600 font-bold mb-2"
-            >
-              Priority:
-            </label>
-            <select
-              id="priority"
-              value={priority}
-              onChange={handlePriorityChange}
-              className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-              required
-            >
-              <option value="" disabled>
-                Select priority
-              </option>
-              <option value="P1">P1</option>
-              <option value="P2">P2</option>
-              <option value="P3">P3</option>
-              <option value="P4">P4</option>
-              <option value="P5">P5</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="impact"
-              className="block text-gray-600 font-bold mb-2"
-            >
-              Impact:
-            </label>
-            <select
-              id="impact"
-              value={impact}
-              onChange={handleImpactChange}
-              className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-              required
-            >
-              <option value="" disabled>
-                Select impact
-              </option>
-              <option value="maintenance">Maintenance</option>
-              <option value="minor">Minor</option>
-              <option value="major">Major</option>
-              <option value="critical">Critical</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="componentIds"
-              className="block text-gray-600 font-bold mb-2"
-            >
-              Select Components (Ctrl+Click for multiple):
-            </label>
-            <select
-              id="componentIds"
-              multiple
-              value={componentIds}
-              onChange={(e) =>
-                setComponentIds(
-                  [...e.target.selectedOptions].map((option) => option.value)
-                )
-              }
-              className="w-full h-32 px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-              required
-            >
-              {components.map((component) => (
-                <option key={component.id} value={component.id}>
-                  {component.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="notifications"
-              className="block text-gray-600 font-bold mb-2"
-            >
-              Send Notifications?
-            </label>
-            <input
-              type="checkbox"
-              id="notifications"
-              checked={notifications}
-              onChange={() => setNotifications(!notifications)}
-              className="mr-2 leading-tight"
-            />
-            <span className="text-gray-700">Yes</span>
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="outageType"
-              className="block text-gray-600 font-bold mb-2"
-            >
-              Outage Type:
-            </label>
-            <select
-              id="outageType"
-              value={outageType}
-              onChange={handleOutageTypeChange}
-              className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-              required
-            >
-              <option value="" disabled>
-                Select outage type
-              </option>
-              <option value="operational">Operational</option>
-              <option value="under_maintenance">Under Maintenance</option>
-              <option value="degraded_performance">Degraded Performance</option>
-              <option value="partial_outage">Partial Outage</option>
-              <option value="major_outage">Major Outage</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="status"
-              className="block text-gray-600 font-bold mb-2"
-            >
-              Status:
-            </label>
-            <select
-              id="status"
-              value={status}
-              onChange={handleStatusChange}
-              className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-              required
-            >
-              <option value="" disabled>
-                Select status
-              </option>
-              <option value="investigating">Investigating</option>
-              <option value="identified">Identified</option>
-              <option value="monitoring">Monitoring</option>
-              <option value="resolved">Resolved</option>
-            </select>
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none hover:bg-blue-700"
-          >
-            Create Incident
-          </button>
-        </form>
+        <CreateIncidentForm />
       </div>
     </div>
   );
