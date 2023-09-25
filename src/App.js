7cec1e0a44a20799cc7a2b9ef9b336354dabc357
Stefan Hall <stefan.hall@cfainstitute.org>
2023-09-25 11:43:31 -0400

    remove auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import { authProvider } from './authProvider';
-import { AzureAD, AuthenticationState } from 'react-aad-msal';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CreateIncidentPage from "./incident_form.js";
 import IncidentUpdateForm from "./incident_update";
@@ -24,50 +22,9 @@ const router = createBrowserRouter([
 
 
 function App() {
- 
   return (
-    
     <div>
-
- 
- <AzureAD provider={authProvider} forceLogin={true}>
-  {
-    
-    ({login, logout, authenticationState, error, accountInfo}) => {
-      switch (authenticationState) {
-        case AuthenticationState.Authenticated:
-          return (
-            <div>
-            <div>
-              <span>Welcome, {accountInfo.account.userName}!</span>
-              <button 
-              className="px-3 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-              onClick={logout}>Logout</button>
-              </div>
-              <AzureAD provider={authProvider}>
-            <RouterProvider router={router} />
-            </AzureAD>
-            </div>
-            
-          );
-        case AuthenticationState.Unauthenticated:
-          return (
-            <div>
-              {error && <p><span>An error occured during authentication, please try again!</span></p>}
-              <p>
-                <span>Hey stranger, you look new!</span>
-                <button 
-                className="px-3 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-                onClick={login}>Login</button>
-              </p>
-            </div>
-          );
-        case AuthenticationState.InProgress:
-          return (<p>Authenticating...</p>);
-      }
-    }
-  }
-</AzureAD>
+      <RouterProvider router={router} />
     </div>
   );
 }
